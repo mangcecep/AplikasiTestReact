a43e514818ee0365f76dbd29c2f7a93b4b26b1e0
stephanie.mulyono@foundry.qreatiq.com <cecepsolihinyusup@gmail.com>
2020-07-08 07:12:14 +0700

    fix mobile view and font style premium pages

--- src/Pages/PremiumPage/PremiumPage.js
+++ src/Pages/PremiumPage/PremiumPage.js
@@ -1,26 +1,48 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
+import styled from "styled-components";
 import { Look211pngImage } from "../../components/images";
+
+const Premium = styled.div`
+  @font-face {
+    font-family: "Poppins Regular";
+    src: local("Poppins Regular"),
+      url(../../assets/fonts/Poppins-Regular.ttf) format("truetype");
+  }
+  font-family: "Poppins Regular";
+  @media screen and (max-width: 420px) {
+    .pict {
+      display: none;
+    }
+    .text-dark {
+      margin-top: 10px;
+      text-align: center;
+    }
+    h5,
+    h2 {
+      font-size: 15px;
+    }
+  }
+`;
 
 const PremiumPage = () => {
   return (
-    <div className="container">
+    <Premium className="container">
       <Navbar loginProp={{ isLogin: true }} />
-      <section className="jumbotron">
+      <section className="container bg-white text-dark">
         <div className="header text-center">
           <h2>Halaman detail PREMIUM Anda</h2>
           <p>
             untuk 10 lembar detail informasi profil Anda, yang Anda bisa
-            manfaatkan <br />
-            untuk kemajuan diri, pekerjaan, dan bisnis Anda
+            manfaatkan untuk kemajuan diri, pekerjaan, dan bisnis Anda
           </p>
         </div>
       </section>
-      <section className="jumbotron">
+      <section className="container bg-white">
         <div className="row clearfix">
           <div className="col-lg-9 col-md-9 col-sm-9 my-auto">
-            <h2>Apa saja yang akan Saya Peroleh?</h2>
+            <h2 className="text-dark">Apa saja yang akan Saya Peroleh?</h2>
             <br />
             <h5 className="alert alert-danger">
               Anda akan memperoleh hasil laporan detail tentang diri Anda
@@ -28,7 +50,11 @@ const PremiumPage = () => {
           </div>
           <br />
           <div className="col-lg-3 col-md-3 col-sm-3 mb-2">
-            <img src={Look211pngImage.url} alt={Look211pngImage.alt} />
+            <img
+              src={Look211pngImage.url}
+              alt={Look211pngImage.alt}
+              className="pict"
+            />
           </div>
           <div className="col-lg-12 col-md-12 col-sm-12">
             <h5 className="alert alert-danger">
@@ -53,7 +79,7 @@ const PremiumPage = () => {
         </div>
       </section>
       <Footer />
-    </div>
+    </Premium>
   );
 };
 
